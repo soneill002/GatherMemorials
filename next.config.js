af69b34a18f3dd9ddd459ba63a94af2757c2dbaa
soneill002 <soneill002@gmail.com>
2025-08-26 08:36:00 -0400

    restart

--- next.config.js
+++
@@ -1,9 +0,0 @@
-// Next.js configuration file. You can keep this minimal at first.
-// We’ll add image domains, headers, and other settings later.
-
- /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true, // Helps catch potential issues during development
-}
-
-module.exports = nextConfig
