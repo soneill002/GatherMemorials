6251341d8b0f685b3b508f16dac5bcf17efd4341
soneill002 <soneill002@gmail.com>
2025-09-05 11:18:32 -0400

    update

--- app/auth/reset-password/page.tsx
+++ app/auth/reset-password/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-
+export const dynamic = 'force-dynamic';
 import { useState, FormEvent, useEffect } from 'react';
 import Link from 'next/link';
 import { useRouter, useSearchParams } from 'next/navigation';
--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-
+export const dynamic = 'force-dynamic';
 import { useState, FormEvent } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
