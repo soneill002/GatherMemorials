338d7af91cf23b688e8c192f8e1c57ccdce59fb5
soneill002 <soneill002@gmail.com>
2025-09-10 22:16:24 -0400

    update

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -218,14 +218,7 @@ export function Navbar() {
                           My Dashboard
                         </Link>
                         
-                        <Link
-                          href="/account"  // Changed from "/account/memorials" to "/account"
-                          className="flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-50 transition-colors"
-                          onClick={() => setShowDropdown(false)}
-                        >
-                          <FileTextIcon className="w-4 h-4 mr-3 text-gray-400" />
-                          My Memorials
-                        </Link>
+                        {/* REMOVED MY MEMORIALS LINK - It was redundant */}
                         
                         <Link
                           href="/account/prayer-list"
@@ -356,14 +349,9 @@ export function Navbar() {
                   <UserIcon className="w-5 h-5 mr-3" />
                   My Dashboard
                 </Link>
-                <Link
-                  href="/account"  // Also updated in mobile menu - points to dashboard
-                  className="flex items-center px-3 py-2 text-base font-medium text-gray-700 hover:bg-gray-50 rounded-md"
-                  onClick={() => setMobileMenuOpen(false)}
-                >
-                  <FileTextIcon className="w-5 h-5 mr-3" />
-                  My Memorials
-                </Link>
+                
+                {/* REMOVED MY MEMORIALS LINK FROM MOBILE MENU AS WELL */}
+                
                 <Link
                   href="/account/prayer-list"
                   className="flex items-center px-3 py-2 text-base font-medium text-gray-700 hover:bg-gray-50 rounded-md"
